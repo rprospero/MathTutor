5d90ec081fdf0e9a8ec771058f66cf9b19fcd646
Adam Washington <adlwashi@indiana.edu>
2012-05-08 21:41:49 -0400

    Refactored most binary operation code into a BinaryOp class.

--- model.js
+++ model.js
@@ -17,47 +17,61 @@ var Number = Object.create(Base);
 
 Number.type = "Number";
 Number.value = 0;
+Number.precedence = 9000;
 Number.toString = function () {
     return "" + this.value;
 };
 
-var Plus = Object.create(Base);
-Plus.type = "Plus";
-Plus.left = Object.create(Number);
-Plus.right = Object.create(Number);
-Plus.combine = function () {
-    var result = {success: false,
-                  message: "Cannot add non-Numbers",
-                  node: this};
-    if (this.left.type === "Number" && this.right.type === "Number") {
-        var x = Object.create(Number);
-        x.value = this.left.value + this.right.value;
-        result.node = x;
-        result.success = true;
-        result.message = "Added";
-    }
+function makeNumber (n) {
+    var result = Object.create(Number);
+    result.value = n;
     return result;
 };
-Plus.toString = function () {
-    return this.left.toString() + " + " + this.right.toString();
-}
 
-var Mult = Object.create(Plus);
-Mult.type = "Mult"
-Mult.combine = function () {
+var BinaryOp = Object.create(Base);
+BinaryOp.type = "BinaryOp";
+BinaryOp.precedence = -1;
+BinaryOp.left = Object.create(Number);
+BinaryOp.right = Object.create(Number);
+BinaryOp.op = function (x,y) { return x; }
+BinaryOp.combine = function () {
     var result = {success: false,
                   message: "Cannot add non-Numbers",
                   node: this};
     if (this.left.type === "Number" && this.right.type === "Number") {
         var x = Object.create(Number);
-        x.value = this.left.value * this.right.value;
+        x.value = this.op(this.left.value,this.right.value);
         result.node = x;
         result.success = true;
-        result.message = "Added";
+        result.message = "Combined";
+    }
+    return result;
+}; 
+BinaryOp.text = "op";
+BinaryOp.toString = function () {
+    var result = "";
+    if (this.left.precedence < this.precedence) {
+	result = "( " + this.left.toString() + " )";
+    } else {
+	result = this.left.toString();
+    }
+    result += " " + this.text + " ";
+    if (this.right.precedence < this.precedence) {
+	result += "( " + this.right.toString() + " )";
+    } else {
+	result += this.right.toString();
     }
     return result;
 };
-Mult.toString = function () {
-    //FIXME:  This needs parentheses when multiplying addition
-    return this.left.toString() + " * " + this.right.toString();
-}
+
+var Plus = Object.create(BinaryOp);
+Plus.type = "Plus";
+Plus.op = function (x,y) { return x+y;};
+Plus.text = "+"
+Plus.precedence = 0
+
+var Mult = Object.create(BinaryOp);
+Mult.type = "Mult";
+Mult.op = function (x,y) { return x*y;};
+Mult.text = "*"
+Mult.precedence = 1
