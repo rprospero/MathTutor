1c16cd091f0ed1839067a4af490c8c406c9bcaf5
Adam Washington <adlwashi@indiana.edu>
2012-05-08 21:22:34 -0400

    Added toString functionality

--- model.js
+++ model.js
@@ -11,11 +11,15 @@ if (typeof Object.create !== 'function') {
 }
 
 var Base = {type: "Node"};
+Base.toString = function () {return "Error";};
 
 var Number = Object.create(Base);
 
 Number.type = "Number";
 Number.value = 0;
+Number.toString = function () {
+    return "" + this.value;
+};
 
 var Plus = Object.create(Base);
 Plus.type = "Plus";
@@ -34,3 +38,26 @@ Plus.combine = function () {
     }
     return result;
 };
+Plus.toString = function () {
+    return this.left.toString() + " + " + this.right.toString();
+}
+
+var Mult = Object.create(Plus);
+Mult.type = "Mult"
+Mult.combine = function () {
+    var result = {success: false,
+                  message: "Cannot add non-Numbers",
+                  node: this};
+    if (this.left.type === "Number" && this.right.type === "Number") {
+        var x = Object.create(Number);
+        x.value = this.left.value * this.right.value;
+        result.node = x;
+        result.success = true;
+        result.message = "Added";
+    }
+    return result;
+};
+Mult.toString = function () {
+    //FIXME:  This needs parentheses when multiplying addition
+    return this.left.toString() + " * " + this.right.toString();
+}
